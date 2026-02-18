836dd49b47afac24bb26269e9ab9335b3e606453
Ashruth Reddy Gangula <ashruth671@gmail.com>
2026-02-18 02:13:34 -0500

    update models.go file

--- backend/models.go
+++ backend/models.go
@@ -16,3 +16,30 @@ type User struct {
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
+
+type Post struct {
+	ID        string `json:"id" gorm:"primaryKey"`
+	UserID    string `json:"user_id"`
+	User      User   `json:"user" gorm:"foreignKey:UserID"`
+	Title     string `json:"title"`
+	Content   string `json:"content"`
+	ImageURL  string `json:"image_url"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type Comment struct {
+	ID        string `json:"id" gorm:"primaryKey"`
+	PostID    string `json:"post_id"`
+	UserID    string `json:"user_id"`
+	User      User   `json:"user" gorm:"foreignKey:UserID"`
+	Content   string `json:"content"`
+	CreatedAt time.Time
+}
+
+type Like struct {
+	ID     string `json:"id" gorm:"primaryKey"`
+	PostID string `json:"post_id"`
+	UserID string `json:"user_id"`
+	User   User   `json:"user" gorm:"foreignKey:UserID"`
+}
